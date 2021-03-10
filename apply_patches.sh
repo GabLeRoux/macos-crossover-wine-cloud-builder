f05d4961795f3ed6bad75877f1cc22c3d5ac18a6
dasmy <win_wa@gmx.net>
2021-03-10 18:08:11 +0100

    fix build of comctl32 module

--- apply_patches.sh
+++ apply_patches.sh
@@ -7,3 +7,5 @@ SCRIPT_DIR="`dirname \"${BASH_SOURCE}\"`"
 cp ${SCRIPT_DIR}/distversion.h sources/wine/include/distversion.h
 
 # patch sources/wine/dlls/winecoreaudio.drv/authorization.m < ${SCRIPT_DIR}/winecoreaudio.patch
+
+patch sources/wine/dlls/comctl32/propsheet.c < ${SCRIPT_DIR}/comctl32_propsheet.patch
