c2d51268b2978a95b56e6aa9586f53b6b04394a6
dasmy <win_wa@gmx.net>
2021-03-10 18:32:26 +0100

    more patches

--- apply_patches.sh
+++ apply_patches.sh
@@ -8,4 +8,8 @@ cp ${SCRIPT_DIR}/distversion.h sources/wine/include/distversion.h
 
 # patch sources/wine/dlls/winecoreaudio.drv/authorization.m < ${SCRIPT_DIR}/winecoreaudio.patch
 
+patch sources/wine/include/msvcrt/stdio.h < ${SCRIPT_DIR}/msvcrt_stdio.patch
+
 patch sources/wine/dlls/comctl32/propsheet.c < ${SCRIPT_DIR}/comctl32_propsheet.patch
+patch sources/wine/dlls/comctl32/taskdialog.c < ${SCRIPT_DIR}/comctl32_taskdialog.patch
+patch sources/wine/dlls/comctl32/treeview.c < ${SCRIPT_DIR}/comctl32_treeview.patch
