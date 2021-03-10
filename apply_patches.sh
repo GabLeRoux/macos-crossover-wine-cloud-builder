c609e3f446b1ddf3107f947d06c04ddf25c7f65f
dasmy <win_wa@gmx.net>
2021-03-10 20:33:26 +0100

    more patches

--- apply_patches.sh
+++ apply_patches.sh
@@ -13,3 +13,9 @@ patch sources/wine/include/msvcrt/stdio.h < ${SCRIPT_DIR}/msvcrt_stdio.patch
 patch sources/wine/dlls/comctl32/propsheet.c < ${SCRIPT_DIR}/comctl32_propsheet.patch
 patch sources/wine/dlls/comctl32/taskdialog.c < ${SCRIPT_DIR}/comctl32_taskdialog.patch
 patch sources/wine/dlls/comctl32/treeview.c < ${SCRIPT_DIR}/comctl32_treeview.patch
+
+patch sources/wine/dlls/comdlg32/filedlg.c < ${SCRIPT_DIR}/comdlg32_filedlg.patch
+patch sources/wine/dlls/comdlg32/filedlgbrowser.c < ${SCRIPT_DIR}/comdlg32_filedlgbrowser.patch
+patch sources/wine/dlls/comdlg32/printdlg.c < ${SCRIPT_DIR}/comdlg32_printdlg.patch
+
+patch sources/wine/dlls/cryptui/main.c < ${SCRIPT_DIR}/cryptui_main.patch
