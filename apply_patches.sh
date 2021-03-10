ce49c17b69b376958e5705af0f200d529a64dd1c
dasmy <win_wa@gmx.net>
2021-03-10 15:07:53 +0100

    disable the patch for now - I think, this has to be fixed using proper compiler arguments

--- apply_patches.sh
+++ apply_patches.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR="`dirname \"${BASH_SOURCE}\"`"
 
 cp ${SCRIPT_DIR}/distversion.h sources/wine/include/distversion.h
 
-patch sources/wine/dlls/winecoreaudio.drv/authorization.m < ${SCRIPT_DIR}/winecoreaudio.patch
+# patch sources/wine/dlls/winecoreaudio.drv/authorization.m < ${SCRIPT_DIR}/winecoreaudio.patch
