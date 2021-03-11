66625f467d7d501288318a485b070035c836f729
dasmy <win_wa@gmx.net>
2021-03-11 08:56:38 +0100

    move patches into subdir

--- apply_patches.sh
+++ apply_patches.sh
@@ -4,18 +4,18 @@ set -ex
 
 SCRIPT_DIR="`dirname \"${BASH_SOURCE}\"`"
 
-cp ${SCRIPT_DIR}/distversion.h sources/wine/include/distversion.h
+cp ${SCRIPT_DIR}/patches/distversion.h sources/wine/include/distversion.h
 
 # patch sources/wine/dlls/winecoreaudio.drv/authorization.m < ${SCRIPT_DIR}/winecoreaudio.patch
 
-patch sources/wine/include/msvcrt/stdio.h < ${SCRIPT_DIR}/msvcrt_stdio.patch
+patch sources/wine/include/msvcrt/stdio.h < ${SCRIPT_DIR}/patches/msvcrt_stdio.patch
 
-patch sources/wine/dlls/comctl32/propsheet.c < ${SCRIPT_DIR}/comctl32_propsheet.patch
-patch sources/wine/dlls/comctl32/taskdialog.c < ${SCRIPT_DIR}/comctl32_taskdialog.patch
-patch sources/wine/dlls/comctl32/treeview.c < ${SCRIPT_DIR}/comctl32_treeview.patch
+patch sources/wine/dlls/comctl32/propsheet.c < ${SCRIPT_DIR}/patches/comctl32_propsheet.patch
+patch sources/wine/dlls/comctl32/taskdialog.c < ${SCRIPT_DIR}/patches/comctl32_taskdialog.patch
+patch sources/wine/dlls/comctl32/treeview.c < ${SCRIPT_DIR}/patches/comctl32_treeview.patch
 
-patch sources/wine/dlls/comdlg32/filedlg.c < ${SCRIPT_DIR}/comdlg32_filedlg.patch
-patch sources/wine/dlls/comdlg32/filedlgbrowser.c < ${SCRIPT_DIR}/comdlg32_filedlgbrowser.patch
-patch sources/wine/dlls/comdlg32/printdlg.c < ${SCRIPT_DIR}/comdlg32_printdlg.patch
+patch sources/wine/dlls/comdlg32/filedlg.c < ${SCRIPT_DIR}/patches/comdlg32_filedlg.patch
+patch sources/wine/dlls/comdlg32/filedlgbrowser.c < ${SCRIPT_DIR}/patches/comdlg32_filedlgbrowser.patch
+patch sources/wine/dlls/comdlg32/printdlg.c < ${SCRIPT_DIR}/patches/comdlg32_printdlg.patch
 
-patch sources/wine/dlls/cryptui/main.c < ${SCRIPT_DIR}/cryptui_main.patch
+patch sources/wine/dlls/cryptui/main.c < ${SCRIPT_DIR}/patches/cryptui_main.patch
