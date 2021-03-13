fc6f824f8617a33b7a74873f775cf18172ea0821
dasmy <win_wa@gmx.net>
2021-03-13 17:42:27 +0100

    use variable instead of hardcoded file name

--- get_source.sh
+++ get_source.sh
@@ -8,12 +8,12 @@ if [ -z "$CROSS_OVER_VERSION" ]; then
 fi
 
 CROSS_OVER_SOURCE_URL=https://media.codeweavers.com/pub/crossover/source/crossover-sources-${CROSS_OVER_VERSION}.tar.gz
-CROSS_OVER_LOCAL_FILE=crossover.tar.gz
+CROSS_OVER_LOCAL_FILE=crossover_${CROSS_OVER_VERSION}.tar.gz
 
 if [ ! -f "${CROSS_OVER_LOCAL_FILE}" ]; then
     wget -O ${CROSS_OVER_LOCAL_FILE} ${CROSS_OVER_SOURCE_URL}
 fi
 
 if [ ! -d "sources" ]; then
-    tar xf crossover.tar.gz
+    tar xf ${CROSS_OVER_LOCAL_FILE}.tar.gz
 fi
