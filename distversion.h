ccd887aa9feefa32cb8c7fa0c9deb33dd8cdbfd8
dasmy <win_wa@gmx.net>
2021-04-09 17:41:25 +0200

    do not direct potential users to filing a ticket with codeweavers if anything goes wrong...

--- distversion.h
+++ distversion.h
@@ -7,6 +7,6 @@
 * its way into the wine tree.
 * --------------------------------------------------------------- */
 
-#define WINDEBUG_WHAT_HAPPENED_MESSAGE "This can be caused by a problem in the program or a deficiency in Wine. You may want to check <a href=\"http://www.codeweavers.com/compatibility/\">http://www.codeweavers.com/compatibility/</a> for tips about running this application."
+#define WINDEBUG_WHAT_HAPPENED_MESSAGE "This can be caused by a problem in the program or a deficiency in Wine. You may want to check <a href=\"http://www.codeweavers.com/compatibility/\">http://www.codeweavers.com/compatibility/</a> for tips about running this application. As this is an unofficial build, do not expect to get any support, though."
 
-#define WINDEBUG_USER_SUGGESTION_MESSAGE "If this problem is not present under Windows and has not been reported yet, you can save the detailed information to a file using the \"Save As\" button, then <a href=\"http://www.codeweavers.com/support/tickets/enter/\">file a bug report</a> and attach that file to the report."
+#define WINDEBUG_USER_SUGGESTION_MESSAGE "If this problem is not present under Windows and has not been reported yet, you can save the detailed information to a file using the \"Save As\" button and try to find out more via elaborate internet search. As this is an unofficial build, do not expect to get any support, though."
